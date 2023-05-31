adbdd21c3b709b8f6a5330b9109f13fd79f1f44f
jinyoung1018 <jywsb1816@gmail.com>
2023-05-31 13:59:43 +0900

    시계만들기

--- src/index.js
+++ src/index.js
@@ -1,15 +1,30 @@
 import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+// import ReactDOM from 'react-dom/client';
+import {createRoot} from 'react-dom/client';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+import './index.css';
+// import App from './App';
+import reportWebVitals from './reportWebVitals';
+import Clock from './chapter_04/Clock';
+
+
+const root = createRoot(document.getElementById('root'));
+// root.render(
+//   <React.StrictMode>
+//     <App />
+//   </React.StrictMode>
+// );
+
+setInterval( () => {
+  root.render(
+    <React.StrictMode>
+    <Clock />
+  </React.StrictMode>,
+  document.getElementById('root')
+  );
+}, 1000);
+
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
